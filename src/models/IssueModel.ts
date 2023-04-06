dd5bc369784055e84add0cd8f8cdac2334df7f84
Aadv1k <aadv1k@outlook.com>
2023-04-06 21:03:50 +0530

    Implement IssueModel (untested)

--- src/models/IssueModel.ts
+++ src/models/IssueModel.ts
@@ -12,18 +12,18 @@ export default class IssueModel {
       database: DBConfig.DB_NAME,
       password: DBConfig.PASSWORD,
       port: DBConfig.PORT,
-      ssl: true
-    })
+      ssl: true,
+    });
   }
 
   async init(): Promise<void> {
     try {
       await this.client.connect();
-
       await this.client.query(`
         CREATE TABLE issues IF NOT EXISTS (
-         userid UUID NOT NULL
-         issueid UUID NOT NULL
+         id UUID NOT NULL
+         lenderid UUID NOT NULL
+         borrowerid uuid NOT NULL
          bookid UUID NOT NULL
         )
         `);
@@ -32,13 +32,53 @@ export default class IssueModel {
     }
   }
 
-  async pushIssue(data: Issue) { /* TODO */}
-  async removeIssue(data: Issue) { /* TODO */}
-  async getIssue(data: Issue) { /* TODO */}
+  async pushIssue(data: Issue): Promise<Issue | null> {
+    try {
+      await this.client.query(
+        "INSERT INTO issues (id, lenderid, borrowerid, bookid) VALUES ($1 $2 $3 $4)",
+        [data.id, data.lenderId, data.borrowerId, data.bookId]
+      );
+      return data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
+  async removeIssue(
+    issueid: string,
+    borrowerid?: string,
+    lenderid?: string
+  ): Promise<void | null> {
+    try {
+      await this.client.query(
+        "DELETE FROM issues WHERE issueid = $1 OR borrowerid = $2 OR lenderid = $3",
+        [issueid ?? "", borrowerid ?? "", lenderid ?? ""]
+      );
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
+  async getIssue(
+    issueid: string,
+    borrowerid?: string,
+    lenderid?: string
+  ): Promise<void | null> {
+    try {
+      let response = await this.client.query(
+        "SELECT * FROM issues WHERE issueid = $1 OR borrowerid = $2 OR lenderid = $3",
+        [issueid ?? "", borrowerid ?? "", lenderid ?? ""]
+      );
+      return response.rows[0];
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
   async close(): Promise<void> {
     await this.client.end();
   }
-
 }
